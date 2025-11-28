333515cba71a3a42343ff17d24eed3d3f26ffc03
Krytonix <vrishankkasturi914@gmail.com>
2025-11-28 22:17:03 +0530

    Add files via upload

---
+++ editor.js
@@ -0,0 +1,15 @@
+// Stored in localStorage:
+{
+  "quantum_users": [
+    {
+      "id": "123456789",
+      "username": "SteveMiner",
+      "passwordHash": "-1234567890",
+      "email": "steve@example.com",
+      "created": "2025-11-28T20:30:00.000Z",
+      "plan": "miner"
+    }
+  ],
+  "quantum_session": { /* current logged in user */ },
+  "quantum_project_{userId}": { /* user's projects */ }
+}
